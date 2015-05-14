3709edf84dbe49c44216e5f764b4f662a6ca312e
AnthonyJMyers <nefarious.myers@gmail.com>
2015-05-14 15:15:09 -0700

    CommitA4

---
+++ Assignment4/App_Code/VenueLoginRegistrationService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "VenueLoginRegistrationService" in code, svc and config file together.
+public class VenueLoginRegistrationService : IVenueLoginRegistrationService
+{
+    ShowTrackerEntities db = new ShowTrackerEntities();
+    public bool RegisterVenue(Venue v, VenueLogin vl)
+    {
+        bool result = true;
+        try
+        {
+            PasswordHash ph = new PasswordHash();
+            KeyCode kc = new KeyCode();
+            int code = kc.GetKeyCode();
+            byte[] hashed = ph.HashIt(vl.VenueLoginPasswordPlain, code.ToString());
+
+            Venue ven = new Venue();
+            ven.VenueName = v.VenueName;
+            ven.VenueEmail = v.VenueEmail;
+            ven.VenueAddress = v.VenueAddress;
+            ven.VenueAgeRestriction = v.VenueAgeRestriction;
+            ven.VenueCity = v.VenueCity;
+            ven.VenueDateAdded = DateTime.Now;
+            ven.VenuePhone = v.VenuePhone;
+            ven.VenueState = v.VenueState;
+            ven.VenueWebPage = v.VenueWebPage;
+            ven.VenueZipCode = v.VenueZipCode;
+            ven.VenueLogins = v.VenueLogins;
+            ven.VenueKey = v.VenueKey;
+
+          
+
+            VenueLogin venl = new VenueLogin();
+            venl.Venue = ven;
+            venl.VenueLoginDateAdded = DateTime.Now;
+            venl.VenueLoginHashed = hashed;
+            venl.VenueLoginKey = code;
+            venl.VenueLoginPasswordPlain = vl.VenueLoginPasswordPlain;
+            venl.VenueLoginUserName = vl.VenueLoginUserName;
+            venl.VenueLoginRandom = vl.VenueLoginRandom;
+            venl.VenueKey = vl.VenueKey;
+
+            db.Venues.Add(ven);
+            db.VenueLogins.Add(venl);
+            db.SaveChanges();
+
+            }
+        catch
+        {
+            result = false;
+        }
+        return result;
+    }
+
+    public int VenueLogin(string userName, string Password)
+    {
+        int id = 0;
+
+        LoginClass lc = new LoginClass(Password, userName);
+        id = lc.ValidateLogin();
+
+        return id;
+    }
+}
---
+++ Assignment4pt2/App_Code/NewShowService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "NewShowService" in code, svc and config file together.
+public class NewShowService : INewShowService
+{
+
+    ShowTrackerEntities db = new ShowTrackerEntities();
+
+    public bool AddArtist(Artist a)
+    {
+        bool result = true;
+        try
+        {
+            Artist ar = new Artist();
+            ar.ArtistName = a.ArtistName;
+            db.Artists.Add(ar);
+            db.SaveChanges();
+        }
+        catch{
+            result = false;
+        }
+        return result;
+    }
+
+    public bool AddShow(Show s, ShowDetail sd)
+    {
+        bool result = true;
+        try
+        {
+            Show shw = new Show();
+            shw.ShowName = s.ShowName;
+            shw.ShowDate = s.ShowDate;
+            shw.ShowDateEntered = DateTime.Now;
+            shw.ShowDetails = s.ShowDetails;
+            shw.ShowKey = s.ShowKey;
+            shw.ShowTicketInfo = s.ShowTicketInfo;
+            shw.ShowTime = s.ShowTime;
+            shw.VenueKey = s.VenueKey;
+
+            ShowDetail shwd = new ShowDetail();
+            shwd.Artist = sd.Artist;
+            shwd.ArtistKey = sd.ArtistKey;
+            shwd.Show = shw;
+            shwd.ShowDetailAdditional = sd.ShowDetailAdditional;
+            shwd.ShowDetailArtistStartTime = sd.ShowDetailArtistStartTime;
+            shwd.ShowDetailKey = sd.ShowDetailKey;
+            shwd.ShowKey = sd.ShowKey;
+
+            db.Shows.Add(shw);
+            db.ShowDetails.Add(shwd);
+            db.SaveChanges();
+        }
+        catch
+        {
+            result = false;
+        }
+        return result;
+    }
+
+    public List<Artist> GetArtists()
+    {
+        var tls = from b in db.Artists
+                  orderby b.ArtistName
+                  select b;
+
+        List<Artist> titles = new List<Artist>();
+        foreach (var t in tls)
+        {
+            Artist b = new Artist();
+            b.ArtistName = t.ArtistName;
+            b.ArtistEmail = t.ArtistEmail;
+            b.ArtistKey = t.ArtistKey;
+            b.ArtistWebPage = t.ArtistWebPage;
+            b.ShowDetails = t.ShowDetails;
+            b.ArtistDateEntered = b.ArtistDateEntered;
+            titles.Add(b);
+        }
+        return titles;
+    }
+}
